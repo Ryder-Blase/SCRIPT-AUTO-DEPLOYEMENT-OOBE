9cf27d3f1d9ab5b504bc6321af45504d7612658c
Ryder B. <153897299+Ryder-Blase@users.noreply.github.com>
2025-05-25 15:11:02 +0200

    Update COMPACTOS_XPRESS16K.bat

--- COMPACTOS_XPRESS16K.bat
+++ COMPACTOS_XPRESS16K.bat
@@ -1,2 +1,2 @@
 echo Compacting Windows... (Optional use more CPU)
-NSudo.exe -U:T -P:E cmd.exe /c compact /s:C:\ /c /a /i /f /EXE:XPRESS16K
+NSudo.exe -U:T -P:E cmd.exe /c compact /s:C:\Windows /c /a /i /f /EXE:XPRESS16K
